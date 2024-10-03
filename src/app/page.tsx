d5cc37844209da91fd493f2ff68c48e372b09725
Rishik Sarkar <rishiksarkar123@gmail.com>
2024-10-03 01:14:42 -0400

    added a lot more lines 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,8 +31,8 @@ export default function Home() {
     <div className="flex flex-col font-[family-name:var(--font-geist-sans)]">
       <section ref={el => { sectionRefs.current[0] = el }} className="section h-screen flex items-center relative">
         <div className="max-w-[calc(70%-20rem)] pl-16 pr-4">
-          <h1 className="text-5xl font-bold mb-4">Rishik Sarkar</h1>
-          <p className="text-xl">
+          <h1 className="text-5xl font-light mb-4">Rishik Sarkar</h1>
+          <p className="text-xl font-light">
             M.Eng. in Computer Science at Cornell University<br />
             B.S. in Computer Science (Honors) at Rutgers University-New Brunswick<br />
             B.S. in Cognitive Science at Rutgers University-New Brunswick<br /><br />
@@ -45,7 +45,7 @@ export default function Home() {
       <section ref={el => { sectionRefs.current[1] = el }} className="section h-screen flex items-center relative">
         <div className="max-w-[calc(70%-20rem)] pl-16 pr-4">
           <h2 className="text-3xl mb-4">About Me</h2>
-          <p className="text-lg leading-relaxed">
+          <p className="text-lg font-light leading-relaxed">
             "If you could train an AI to be a Buddhist, it would probably be pretty good."<br /> - Reid Hoffman<br /><br />
             I am currently pursuing a Master of Engineering in Computer Science at Cornell University. Prior to this, I graduated Summa Cum Laude with honors from Rutgers University-New Brunswick with a B.S. in Computer Science and Cognitive Science and was inducted into Phi Beta Kappa. I also worked as an ML Full-Stack Developer Intern at a fintech scaleup and as a research assistant at a joint Rutgers-Princeton laboratory, where I focused on data integration and machine learning.<br /><br />
             My passions lie at the intersection of artificial intelligence and psychology, and I am keen to explore computational linguistics, cognitive neuropsychiatry, and new LLM frameworks for natural language understanding, including transformer models and fine-tuning for sentiment and emotional analysis. In the future, I aim to establish a startup and develop NLP-driven solutions in the mental healthcare space.
@@ -56,9 +56,9 @@ export default function Home() {
 
       <section ref={el => { sectionRefs.current[2] = el }} className="section h-screen flex items-center relative">
         <div className="pl-16 pr-16">
-          <h2 className="text-3xl mb-4">Resume</h2>
-          <p className="text-xl mb-4">Check out my professional experience and qualifications:</p>
-          <ul className="text-lg list-disc list-inside mb-4">
+          <h2 className="text-3xl mb-4 font-light">Resume</h2>
+          <p className="text-xl mb-4 font-light">Check out my professional experience and qualifications:</p>
+          <ul className="text-lg list-disc list-inside mb-4 font-light">
             <li>View my <a href="#" className="text-gray-300 hover:text-gray-500">Resume</a></li>
             <li>Download my full <a href="#" className="text-gray-300 hover:text-gray-500">CV</a></li>
           </ul>
@@ -68,7 +68,7 @@ export default function Home() {
 
       <section ref={el => { sectionRefs.current[3] = el }} className="section h-screen flex items-center relative">
         <div className="pl-16 pr-16">
-          <h2 className="text-3xl mb-4">Connect With Me</h2>
+          <h2 className="text-3xl mb-4 font-light">Connect With Me</h2>
           {/* Add your connect content here */}
         </div>
         {/* Diagonal line removed */}
--- src/components/NetworkLine.tsx
+++ src/components/NetworkLine.tsx
@@ -8,90 +8,240 @@ const NetworkLine: React.FC = () => {
   const { lines, addLine } = useNetworkLines();
 
   useEffect(() => {
+    // Main Line
     const mainLine = addLine({
       startCoords: { x: window.innerWidth * 0.7, y: 0 },
       endCoords: { x: window.innerWidth * 0.7, y: window.innerHeight },
       tag: 'main-line',
-      // fillSpeed: 100,
       thickness: 2,
-      // nodeLeft: false,
-      // nodeRight: false
     });
 
-    const d1 = addLine({
-      startCoords: { x: window.innerWidth * 0.7, y: window.innerHeight * 0.3 },
-      endCoords: { x: window.innerWidth * 0.6, y: window.innerHeight * 0.5 },
-      tag: 'diagonal-1',
-      // fillSpeed: 2500,
+    // Enter Title
+    addLine({
+      startCoords: { x: window.innerWidth * 0.7, y: window.innerHeight * 0.2 },
+      endCoords: { x: window.innerWidth * 0.6, y: window.innerHeight * 0.4 },
+      tag: 'diagonal',
       thickness: 2,
       nodeLeft: true,
       nodeRight: true,
-      // nextLineId: null
     });
 
-    const h1 = addLine({
-      startCoords: { x: window.innerWidth * 0.6, y: window.innerHeight * 0.5 },
+    addLine({
+      startCoords: { x: window.innerWidth * 0.6, y: window.innerHeight * 0.4 },
       endCoords: { x: window.innerWidth * 0.5, y: window.innerHeight * 0.5 },
-      tag: 'horizontal-1',
-      // fillSpeed: 2500,
+      tag: 'diagonal',
       thickness: 2,
-      // nodeLeft: false,
+      nodeLeft: true,
       nodeRight: true,
-      // previousLineId: d1.id,
-      // nextLineId: null
     });
 
-    const v1 = addLine({
+    addLine({
+      startCoords: { x: window.innerWidth * 0.6, y: window.innerHeight * 0.4 },
+      endCoords: { x: window.innerWidth * 0.5, y: window.innerHeight * 0.6 },
+      tag: 'diagonal',
+      thickness: 2,
+      nodeLeft: true,
+      nodeRight: true,
+    });
+
+    addLine({
+      startCoords: { x: window.innerWidth * 0.6, y: window.innerHeight * 0.4 },
+      endCoords: { x: window.innerWidth * 0.5, y: window.innerHeight * 0.4 },
+      tag: 'horizontal',
+      thickness: 2,
+      nodeRight: true,
+    });
+
+    // Title to About
+    addLine({
       startCoords: { x: window.innerWidth * 0.25, y: window.innerHeight * 0.75 },
       endCoords: { x: window.innerWidth * 0.25, y: window.innerHeight * 0.875 },
-      tag: 'vertical-1',
-      // fillSpeed: 2500,
+      tag: 'vertical',
       thickness: 2,
       nodeLeft: true,
       nodeRight: true,
-      // previousLineId: h1.id,
-      // nextLineId: null
     });
 
-    const v2 = addLine({
+    addLine({
+      startCoords: { x: window.innerWidth * 0.15, y: window.innerHeight * 0.875 },
+      endCoords: { x: window.innerWidth * 0.20, y: window.innerHeight * 0.875 },
+      tag: 'horizontal',
+      thickness: 2,
+      nodeLeft: true,
+      nodeRight: true,
+    });
+
+    addLine({
+      startCoords: { x: window.innerWidth * 0.20, y: window.innerHeight * 0.875 },
+      endCoords: { x: window.innerWidth * 0.25, y: window.innerHeight * 0.875 },
+      tag: 'horizontal',
+      thickness: 2,
+      nodeLeft: true,
+      nodeRight: true,
+    });
+
+    addLine({
       startCoords: { x: window.innerWidth * 0.25, y: window.innerHeight * 0.875 },
-      endCoords: { x: window.innerWidth * 0.25, y: window.innerHeight * 1.0 },
-      tag: 'vertical-2',
-      // fillSpeed: 2500,
+      endCoords: { x: window.innerWidth * 0.175, y: window.innerHeight * 1.0 },
+      tag: 'diagonal',
       thickness: 2,
       nodeLeft: true,
       nodeRight: true,
-      // previousLineId: v1.id,
-      // nextLineId: null
     });
 
-    const h2 = addLine({
+    addLine({
       startCoords: { x: window.innerWidth * 0.25, y: window.innerHeight * 0.875 },
-      endCoords: { x: window.innerWidth * 0.45, y: window.innerHeight * 0.875 },
-      tag: 'horizontal-2',
-      // fillSpeed: 2500,
-      thickness: 2,
-      nodeLeft: true,
-      nodeRight: true,
-      // previousLineId: v1.id,
-      // nextLineId: null
-    });
-
-    const d2 = addLine({
-      startCoords: { x: window.innerWidth * 0.45, y: window.innerHeight * 0.875 },
-      endCoords: { x: window.innerWidth * 0.7, y: window.innerHeight * 1.3 },
-      tag: 'diagonal-2',
-      // fillSpeed: 2500,
+      endCoords: { x: window.innerWidth * 0.225, y: window.innerHeight * 1.0 },
+      tag: 'diagonal',
       thickness: 2,
       nodeLeft: true,
       nodeRight: true,
     });
 
-    // d1.nextLineId = h1.id;
-    // h1.nextLineId = v1.id;
-    // v1.nextLineId = v2.id;
-    // v1.nextLineId = h2.id;
-    // h2.nextLineId = d2.id;
+    addLine({
+      startCoords: { x: window.innerWidth * 0.15, y: window.innerHeight * 0.875 },
+      endCoords: { x: window.innerWidth * 0.175, y: window.innerHeight * 1.0 },
+      tag: 'diagonal',
+      thickness: 2,
+      nodeLeft: true,
+      nodeRight: true,
+    });
+
+    addLine({
+      startCoords: { x: window.innerWidth * 0.15, y: window.innerHeight * 0.875 },
+      endCoords: { x: window.innerWidth * 0.225, y: window.innerHeight * 1.0 },
+      tag: 'diagonal',
+      thickness: 2,
+      nodeLeft: true,
+      nodeRight: true,
+    });
+
+    addLine({
+      startCoords: { x: window.innerWidth * 0.20, y: window.innerHeight * 0.875 },
+      endCoords: { x: window.innerWidth * 0.175, y: window.innerHeight * 1.0 },
+      tag: 'diagonal',
+      thickness: 2,
+      nodeLeft: true,
+      nodeRight: true,
+    });
+
+    addLine({
+      startCoords: { x: window.innerWidth * 0.20, y: window.innerHeight * 0.875 },
+      endCoords: { x: window.innerWidth * 0.225, y: window.innerHeight * 1.0 },
+      tag: 'diagonal',
+      thickness: 2,
+      nodeLeft: true,
+      nodeRight: true,
+    });
+
+    addLine({
+      startCoords: { x: window.innerWidth * 0.20, y: window.innerHeight * 0.75 },
+      endCoords: { x: window.innerWidth * 0.20, y: window.innerHeight * 0.875 },
+      tag: 'vertical',
+      thickness: 2,
+      nodeLeft: true,
+      nodeRight: true,
+    });
+
+    addLine({
+      startCoords: { x: window.innerWidth * 0.15, y: window.innerHeight * 0.75 },
+      endCoords: { x: window.innerWidth * 0.15, y: window.innerHeight * 0.875 },
+      tag: 'vertical',
+      thickness: 2,
+      nodeLeft: true,
+      nodeRight: true,
+    });
+
+    addLine({
+      startCoords: { x: window.innerWidth * 0.15, y: window.innerHeight * 0.75 },
+      endCoords: { x: window.innerWidth * 0.20, y: window.innerHeight * 0.875 },
+      tag: 'diagonal',
+      thickness: 2,
+      nodeLeft: true,
+      nodeRight: true,
+    });
+
+    addLine({
+      startCoords: { x: window.innerWidth * 0.15, y: window.innerHeight * 0.75 },
+      endCoords: { x: window.innerWidth * 0.25, y: window.innerHeight * 0.875 },
+      tag: 'diagonal',
+      thickness: 2,
+      nodeLeft: true,
+      nodeRight: true,
+    });
+
+    addLine({
+      startCoords: { x: window.innerWidth * 0.20, y: window.innerHeight * 0.75 },
+      endCoords: { x: window.innerWidth * 0.15, y: window.innerHeight * 0.875 },
+      tag: 'diagonal',
+      thickness: 2,
+      nodeLeft: true,
+      nodeRight: true,
+    });
+
+    addLine({
+      startCoords: { x: window.innerWidth * 0.20, y: window.innerHeight * 0.75 },
+      endCoords: { x: window.innerWidth * 0.25, y: window.innerHeight * 0.875 },
+      tag: 'diagonal',
+      thickness: 2,
+      nodeLeft: true,
+      nodeRight: true,
+    });
+
+    addLine({
+      startCoords: { x: window.innerWidth * 0.25, y: window.innerHeight * 0.75 },
+      endCoords: { x: window.innerWidth * 0.15, y: window.innerHeight * 0.875 },
+      tag: 'diagonal',
+      thickness: 2,
+      nodeLeft: true,
+      nodeRight: true,
+    });
+
+    addLine({
+      startCoords: { x: window.innerWidth * 0.25, y: window.innerHeight * 0.75 },
+      endCoords: { x: window.innerWidth * 0.20, y: window.innerHeight * 0.875 },
+      tag: 'diagonal',
+      thickness: 2,
+      nodeLeft: true,
+      nodeRight: true,
+    });
+
+    // About
+    addLine({
+      startCoords: { x: window.innerWidth * 0.25, y: window.innerHeight * 0.875 },
+      endCoords: { x: window.innerWidth * 0.475, y: window.innerHeight * 1.0 },
+      tag: 'horizontal',
+      thickness: 2,
+      nodeLeft: true,
+      nodeRight: true,
+    });
+    
+    addLine({
+      startCoords: { x: window.innerWidth * 0.475, y: window.innerHeight * 1.0 },
+      endCoords: { x: window.innerWidth * 0.7, y: window.innerHeight * 1.125 },
+      tag: 'diagonal',
+      thickness: 2,
+      nodeLeft: true,
+      nodeRight: true,
+    });
+
+    addLine({
+      startCoords: { x: window.innerWidth * 0.475, y: window.innerHeight * 1 },
+      endCoords: { x: window.innerWidth * 0.6, y: window.innerHeight * 1.575 },
+      tag: 'diagonal',
+      thickness: 2,
+      nodeLeft: true,
+      nodeRight: true,
+    });
+
+    addLine({
+      startCoords: { x: window.innerWidth * 0.6, y: window.innerHeight * 1.575 },
+      endCoords: { x: window.innerWidth * 0.5, y: window.innerHeight * 2.125 },
+      tag: 'diagonal',
+      thickness: 2,
+      nodeLeft: true,
+      nodeRight: true,
+    });
   }, [addLine]);
 
   return (
